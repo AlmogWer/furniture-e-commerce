4cbc1e6f613bc3e6d4b151bce4f927914927806a
Almog Wertzberger <Almogish@gmail.com>
2021-09-23 17:48:02 +0300

    addtocart setup

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
+  const { AddToCart } = useCartContext();
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
@@ -57,7 +58,11 @@ const AddToCart = ({ product }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to="/cart" className="btn">
+        <Link
+          to="/cart"
+          className="btn"
+          onClick={() => AddToCart(id, mainColor, amount, product)}
+        >
           add to cart
         </Link>
       </div>
--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,23 +1,37 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/cart_reducer'
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/cart_reducer";
 import {
   ADD_TO_CART,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
   CLEAR_CART,
   COUNT_CART_TOTALS,
-} from '../actions'
+} from "../actions";
 
-const initialState = {}
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 534,
+};
 
-const CartContext = React.createContext()
+const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // add to cart
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
+  };
+
   return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
-  )
-}
+    <CartContext.Provider value={{ ...state, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
 // make sure use
 export const useCartContext = () => {
-  return useContext(CartContext)
-}
+  return useContext(CartContext);
+};
